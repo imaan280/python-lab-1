002d14638af5eb7d7813021021daaf1a8bed904d
imaan280 <c4021820@hallam.shu.ac.uk>
2025-11-25 10:13:36 +0000

    commit

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 # Import the flask class and create an instance of it
 from flask import Flask, render_template
+from config import db_config
+import mysql.connector
+from . import main
 
 app = Flask(__name__)
+
+@main.route('/db_data')
+def db_data():
+    # Establish database connection
+    conn = mysql.connector.connect(**db_config)
+    
+    # Create a cursor that returns results as dictionaries
+    cursor = conn.cursor(dictionary=True)
+    
+    # Execute SQL query
+    cursor.execute("SELECT * FROM staff")
+    
+    # Fetch all rows from the query result
+    data = cursor.fetchall()
+    
+    # Clean up resources
+    cursor.close()
+    conn.close()
+    
+    # Pass data to template
+    return render_template('db_data.html', data=data)
 
 # Define a route and view function
 @app.route('/')
